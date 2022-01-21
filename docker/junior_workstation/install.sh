9057f3926218ea7c723aa064d1b0472c5dbe3497
ivljubicic <ivanljubicic36@gmail.com>
2022-01-21 11:16:49 +0100

    dodan docker mail server

---
+++ docker/junior_workstation/install.sh
@@ -0,0 +1,14 @@
+# install visual studio code
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
+sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+rm -f packages.microsoft.gpg
+
+apt install apt-transport-https
+apt update
+apt install -y code -v 1.57.1
+
+# install libre office
+add-apt-repository ppa:libreoffice/ppa
+apt-get update
+apt-get install -y libreoffice
